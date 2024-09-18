8fedb9ee0973b4618e2838c06a7acbbe07011e10
Varshith8868 <konalavasrshith@gmail.com>
2024-09-18 10:02:35 +0530

    MySQL

--- MyFirstDjango/MyFirstDjango/settings.py
+++ MyFirstDjango/MyFirstDjango/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'calc',
+    'myDBApp',
 ]
 
 MIDDLEWARE = [
@@ -76,9 +79,17 @@ WSGI_APPLICATION = 'MyFirstDjango.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MYSQL',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'Varshith@123',
     }
 }
 
